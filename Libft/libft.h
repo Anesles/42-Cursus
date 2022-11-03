7b125372d536b404e572db91b887bb8e2770126d
Bruno Parreira <bbmp2002@gmail.com>
2022-11-03 21:08:33 +0000

    Added more functions

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: brumarti <brumarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 15:15:28 by brumarti          #+#    #+#             */
-/*   Updated: 2022/11/03 13:07:34 by brumarti         ###   ########.fr       */
+/*   Updated: 2022/11/03 21:08:10 by brumarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,9 @@ int			ft_isascii(int c);
 int			ft_isprint(int c);
 int			ft_toupper(int c);
 int			ft_tolower(int c);
+int	ft_strncmp(const char *s1, const char *s2, unsigned int n);
+
+char		*ft_strrchr(const char *s, int c);
 
 size_t		ft_strlen(const char *s);
 size_t		ft_strlcpy(char *dst, const char *src, size_t size);
