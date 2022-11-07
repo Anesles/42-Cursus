b5a299b9a2825ff19468f13b18c82af3d5454fa5
brumarti <brumarti@student.42porto.com>
2022-11-07 18:59:12 +0000

    Update to split

--- Libft/libft/ft_split.c
+++ Libft/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: brumarti <brumarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 18:03:05 by brumarti          #+#    #+#             */
-/*   Updated: 2022/11/07 18:14:55 by brumarti         ###   ########.fr       */
+/*   Updated: 2022/11/07 18:57:37 by brumarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,18 @@ static char	**ft_alloc_array(char const *s, char c, int length)
 
 	i = 0;
 	count = 0;
-	while (s[i] == c)
-		i++;	
-	while (i < length)
+	while (s[i] == c || s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+		i++;
+	while (i < (length + 1))
 	{
 		if ((s[i] == c && s[i - 1] != c))
 			count++;
+		else if (s[i] == 0 && (s[i - 1] != c || s[i - 1] == ' '
+				|| s[i - 1] == '\t' || s[i - 1] == '\n'))
+			count++;
 		i++;
 	}
-	array = (char **)malloc(sizeof(s) * (count + 2));
+	array = (char **)malloc(sizeof(s) * (count + 1));
 	if (!array)
 		return (NULL);
 	return (array);
@@ -44,27 +47,32 @@ static char	*ft_split_range(int start, int finish, char const *s)
 	length = finish - start;
 	str = (char *)malloc(length + 1 * sizeof(*s));
 	while (start < finish)
-		str[i++] = s[start++];
+	{
+		if (s[start] == ' ' || s[start] == '\t' || s[start] == '\n')
+			start++;
+		else
+			str[i++] = s[start++];
+	}
 	str[i] = 0;
 	return (str);
 }
 
 static char	**ft_split_string(char **array, char const *s, char c, int length)
 {
-	int		i;
-	int		j;
-	int		start;
+	int	i;
+	int	j;
+	int	start;
 
 	i = 0;
 	j = 0;
-	while (s[i] == c)
+	while (s[i] == c || s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
 		i++;
 	start = i;
 	while (i < length)
 	{
 		if ((s[i] == c && s[i - 1] != c) || (s[i] != c && s[i + 1] == 0))
 		{
-			if (s[i + 1] == 0)
+			if (s[i + 1] == 0 && s[i] != c)
 				array[j++] = ft_split_range(start, i + 1, s);
 			else
 				array[j++] = ft_split_range(start, i, s);
