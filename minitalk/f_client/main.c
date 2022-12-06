ec85614d0d100b6859038e1203b287379cec2155
brumarti <brumarti@student.42porto.com>
2022-12-06 18:29:58 +0000

    Update

--- minitalk/f_client/main.c
+++ minitalk/f_client/main.c
@@ -6,7 +6,7 @@
 /*   By: brumarti <brumarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 15:50:11 by brumarti          #+#    #+#             */
-/*   Updated: 2022/12/05 17:26:47 by brumarti         ###   ########.fr       */
+/*   Updated: 2022/12/06 18:26:28 by brumarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,32 @@
 
 int main(int argc, char const *argv[])
 {
+	int		pid;
+	int		i;
+	int		j;
+	char	c;
 	
+	if (argc != 3)
+	{
+			ft_printf("Error\nBad number of arguments");
+			exit(EXIT_FAILURE);		
+	}
+	i = 0;
+	pid	= ft_atoi(argv[1]);
+	while (argv[2][i])
+	{
+		j = 7;
+		while (j >= 0)
+		{
+			c = ((argv[2][i] & (1 << j--)) ? '1' : '0');
+			if (c == 1)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(50000);
+		}
+		i++;
+	}
+	ft_printf("\n");
 	return 0;
 }
--- minitalk/f_server/main.c
+++ minitalk/f_server/main.c
@@ -6,13 +6,38 @@
 /*   By: brumarti <brumarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 15:50:11 by brumarti          #+#    #+#             */
-/*   Updated: 2022/12/05 16:06:32 by brumarti         ###   ########.fr       */
+/*   Updated: 2022/12/06 18:26:56 by brumarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 
+void	handler(int	signal)
+{
+	static int				i;
+	static unsigned	char	c;
+	
+	if (!c)
+		c = 0;
+	if (!i)
+		i = 0;
+	c <<=1;
+	c += (signal == SIGUSR1);
+	if (++i == 8)
+	{
+		ft_putchar_fd(c, 1);
+		i = 0;
+		c = 0;
+	}
+	ft_printf("%d\n", i);
+}
+
 int	main(void)
 {
 	ft_printf("PID: %d", getpid());
+	while (1)
+	{
+		signal(SIGUSR1, handler);
+		signal(SIGUSR2, handler);
+	}
 }
