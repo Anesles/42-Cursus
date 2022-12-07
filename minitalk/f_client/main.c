c619bf21058dd1c0961ad7105e968f505b8a3a29
brumarti <bbmp2002@gmail.com>
2022-12-07 00:16:59 +0000

    Finished main functionality

--- minitalk/f_client/main.c
+++ minitalk/f_client/main.c
@@ -6,40 +6,47 @@
 /*   By: brumarti <brumarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 15:50:11 by brumarti          #+#    #+#             */
-/*   Updated: 2022/12/06 18:26:28 by brumarti         ###   ########.fr       */
+/*   Updated: 2022/12/07 00:15:40 by brumarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 
-int main(int argc, char const *argv[])
+static void	encoder(int pid, char *str)
 {
-	int		pid;
-	int		i;
-	int		j;
-	char	c;
-	
-	if (argc != 3)
-	{
-			ft_printf("Error\nBad number of arguments");
-			exit(EXIT_FAILURE);		
-	}
+	int	i;
+	int	j;
+
 	i = 0;
-	pid	= ft_atoi(argv[1]);
-	while (argv[2][i])
+	while (str[i])
 	{
 		j = 7;
 		while (j >= 0)
 		{
-			c = ((argv[2][i] & (1 << j--)) ? '1' : '0');
-			if (c == 1)
+			if (str[i] & (1 << j))
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			usleep(50000);
+			usleep(5);
+			j--;
 		}
 		i++;
 	}
-	ft_printf("\n");
-	return 0;
+}
+
+int	main(int argc, char const *argv[])
+{
+	int		pid;
+	int		i;
+	int		j;
+
+	if (argc != 3)
+	{
+		ft_printf("Error\nBad number of arguments");
+		exit(EXIT_FAILURE);
+	}
+	i = 0;
+	pid = ft_atoi(argv[1]);
+	encoder(pid, argv[2]);
+	return (0);
 }
--- minitalk/f_server/main.c
+++ minitalk/f_server/main.c
@@ -6,35 +6,53 @@
 /*   By: brumarti <brumarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 15:50:11 by brumarti          #+#    #+#             */
-/*   Updated: 2022/12/06 18:26:56 by brumarti         ###   ########.fr       */
+/*   Updated: 2022/12/07 00:13:58 by brumarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 
-void	handler(int	signal)
+static int	decoder(char *str)
 {
-	static int				i;
-	static unsigned	char	c;
-	
+	int	i;
+	int	result;
+
+	result = 0;
+	i = 0;
+	while (str[i])
+	{
+		result <<= 1;
+		if (str[i] == '1')
+			result += 1;
+		i++;
+	}
+	return (result);
+}
+
+static void	handler(int signal)
+{
+	static int	i;
+	static char	*c;
+
 	if (!c)
-		c = 0;
+		c = (char *)malloc(sizeof(char) * 8);
 	if (!i)
 		i = 0;
-	c <<=1;
-	c += (signal == SIGUSR1);
+	if (signal == SIGUSR1)
+		c[i] = '1';
+	else
+		c[i] = '0';
 	if (++i == 8)
 	{
-		ft_putchar_fd(c, 1);
+		ft_printf("%c", decoder(c));
 		i = 0;
 		c = 0;
 	}
-	ft_printf("%d\n", i);
 }
 
 int	main(void)
 {
-	ft_printf("PID: %d", getpid());
+	ft_printf("PID: %d\n", getpid());
 	while (1)
 	{
 		signal(SIGUSR1, handler);
