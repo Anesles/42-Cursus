7534485acc07979059bf07561463d4013eb5976c
brumarti <bbmp2002@gmail.com>
2022-12-07 00:21:43 +0000

    Error fix

--- minitalk/f_client/main.c
+++ minitalk/f_client/main.c
@@ -6,13 +6,13 @@
 /*   By: brumarti <brumarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 15:50:11 by brumarti          #+#    #+#             */
-/*   Updated: 2022/12/07 00:15:40 by brumarti         ###   ########.fr       */
+/*   Updated: 2022/12/07 00:20:44 by brumarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 
-static void	encoder(int pid, char *str)
+static void	encoder(int pid, const char *str)
 {
 	int	i;
 	int	j;
@@ -27,7 +27,7 @@ static void	encoder(int pid, char *str)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			usleep(5);
+			usleep(15);
 			j--;
 		}
 		i++;
@@ -37,15 +37,12 @@ static void	encoder(int pid, char *str)
 int	main(int argc, char const *argv[])
 {
 	int		pid;
-	int		i;
-	int		j;
 
 	if (argc != 3)
 	{
 		ft_printf("Error\nBad number of arguments");
 		exit(EXIT_FAILURE);
 	}
-	i = 0;
 	pid = ft_atoi(argv[1]);
 	encoder(pid, argv[2]);
 	return (0);
