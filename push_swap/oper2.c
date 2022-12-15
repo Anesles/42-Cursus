ddbb1b9bb3188f07009abd1652b709479aa25a70
brumarti <brumarti@student.42porto.com>
2022-12-15 17:08:55 +0000

    Updates

--- push_swap/oper2.c
+++ push_swap/oper2.c
@@ -1,34 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   oper2.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brumarti <brumarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/14 15:38:57 by brumarti          #+#    #+#             */
-/*   Updated: 2022/12/15 17:04:46 by brumarti         ###   ########.fr       */
+/*   Created: 2022/12/15 17:05:16 by brumarti          #+#    #+#             */
+/*   Updated: 2022/12/15 17:06:53 by brumarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int argc, char *argv[])
+void	ra(t_stack *a)
 {
-	t_stack	a;
-	t_stack	b;
+	int	i;
 
-	if (argc > 1)
-	{
-		init(argc, argv, &a, &b);
-		print_stacks(a, b);
-		sa(a.list);
-		print_stacks(a, b);
-		pb(&a, &b);
-		print_stacks(a, b);
-		pb(&a, &b);
-		print_stacks(a, b);
-		pa(&a, &b);
-		print_stacks(a, b);
-	}
-	return 0;
+	i = 0;
+	while ()
 }
